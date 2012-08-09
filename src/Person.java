edf65b56a8ecd5ef358d2f3aade65274085e9dce
yonosuke <ysk@d-suger.com>
2012-08-09 20:26:48 +0900

    second commit

---
+++ src/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+
+	public static String = "uji";
+	public static String = "na";
+
+	public static void main (String args[]){
+			String firstname = null;
+			String lastname = null;
+		public String getfirstname(); 
+		public String setfirstname();
+
+		public String getfirstname() {
+			this.setfirstname = "na";
+			returen = null;
+		}
+
+		public string getlastname() {
+			this.getlastname = "uji"
+			returen = null;
+		};
+
+		public String setfirstname() {
+			this.setfirstname = "na"
+			returen = null;
+		};
+
+		public String setlastname() {
+			this.setfirstname = "uji"
+			returen = null;
+		};
+
+		public String showname(){
+			Uji uji = String(uji);
+			Na na = String(na);
+			Uji uji1 = ("渡辺");
+			Na na1 = ("高志");
+			Uji uji2 = ("和田");
+			Uji na2 =("充史")
+
+
+		log.d(showname(uji1 + na1));
+		log.d(showname(uji2 + na2));
+		};
+
+	};
+
+};
